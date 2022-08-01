961057f4a2d4375eaff9e0493599b738e710eb2d
Gustavo Cortes <gustavocortes981@gmail.com>
2022-08-01 16:52:05 -0300

    Aula 05 - Rota POST
    Criando uma rota POST

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const express = require('express');
+const checklistsRouter = require('./src/routes/checklist')
+
 
 const app = express();
-
 app.use(express.json());
 
-const log = (req, res, next) => {
-    console.log(req.body)
-    console.log(`Data: ${Date.now()}`)
-    next()
-}
-
-app.use(log)
-
-app.get('/', (req, res) => {
-    res.send('<h1>Minha lista de tarefas</h1>')
-})
-
-app.get('/json', (req, res) => {
-    console.log(req.body);
-    res.json({title: 'Tarefa X', done: true})
-} )
+app.use('/checklist', checklistsRouter)
 
 app.listen(3000, () => {
     console.log("Servidor foi iniciado")
 })
-
