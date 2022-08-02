e04e6ae4bd3ef6a66bfc96013152009450dd95c3
Gustavo Cortes <gustavocortes981@gmail.com>
2022-08-02 17:27:02 -0300

    Bulma

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const checklistsRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
 require('./config/database.js')
 
+
+
+
 const app = express();
 app.use(express.json());
+
+app.use(express.static(paht.join(__dirname, 'public')))
 
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
