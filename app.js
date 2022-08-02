f18287930f678ae788940bdda581b80df9e63401
Gustavo Cortes <gustavocortes981@gmail.com>
2022-08-02 14:55:54 -0300

    Mongoose
    Instalação do Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const checklistsRouter = require('./src/routes/checklist')
-
+require('./config/database.js')
 
 const app = express();
 app.use(express.json());
